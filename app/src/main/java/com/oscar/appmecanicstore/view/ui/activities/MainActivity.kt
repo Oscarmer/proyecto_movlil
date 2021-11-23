37bd0d9e2373d637598292d81861963ad8b36336
unknown <oscarmegodmg2002@gmail.com>
2021-11-22 23:15:01 -0500

    Base del proyecto MecanicStore

---
+++ app/src/main/java/com/oscar/appmecanicstore/view/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.oscar.appmecanicstore.view.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oscar.appmecanicstore.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
